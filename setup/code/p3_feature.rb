d781a29f55c1964cc5ed8de15fdd4b60e1ca5dbf
wangchen <wangc1@email.arizona.edu>
2020-04-11 23:28:55 -0700

    Update p3_feature.rb

--- setup/code/p3_feature.rb
+++ setup/code/p3_feature.rb
@@ -4,6 +4,25 @@ type_Str = "It's #{type_Float} am."
 puts "#{type_Str}? No, it's #{type_Int} pm."
 puts "Is it 5 + 3 < 7? #{5 + 3 < 7}"
 
+puts "A small-ish program."
+print "Now Type a Number (Integer): "
+get_int = gets.chomp.to_i
+puts "You input #{get_int} is a integer type.\n"
+
+print " Type a Number (Float): "
+get_flo = gets.chomp.to_f
+puts "You input #{get_flo} is a float type.\n"
+
+print " Type a Sentence (String): "
+get_str = gets.chomp
+puts "You input #{get_str} is a string type.\n"
+
+print "Answer the question: 5 + 3 is less than 7? (yes/no)? "
+get_str1 = gets.chomp
+puts "Your answer is #{get_str1 == "yes"}. This question perform a Boolean type\n"
+
+
+puts "Now show the error type."
 begin
     type_strmore = '*' * 10
     puts type_strmore
@@ -20,3 +39,5 @@ end
 
 type_comp = "*" + 10.0
 puts type_comp
+
+
