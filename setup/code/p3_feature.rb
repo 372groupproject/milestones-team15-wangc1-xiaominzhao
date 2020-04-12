b9a1210d916c3417daa029e8c5ab61e7362710de
wangchen <wangc1@email.arizona.edu>
2020-04-11 23:09:46 -0700

    Update p3_feature.rb

--- setup/code/p3_feature.rb
+++ setup/code/p3_feature.rb
@@ -1,9 +1,22 @@
 type_Int = 10
 type_Float = 9.0
 type_Str = "It's #{type_Float} am."
-# type_Bool = TRUE
-
 puts "#{type_Str}? No, it's #{type_Int} pm."
 puts "Is it 5 + 3 < 7? #{5 + 3 < 7}"
-type_comp = "*"+10.0
+
+begin
+    type_strmore = '*' * 10
+    puts type_strmore
+    type_strmore = '10'.freeze
+    type_strmore << '56789'
+    puts type_strmore
+    
+rescue RuntimeError => e
+    print_exception(e, true)
+rescue => e
+    print_exception(e, false)
+end
+
+
+type_comp = "*" + 10.0
 puts type_comp
