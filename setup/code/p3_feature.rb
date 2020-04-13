58f60add211c019ebf5a4efcf1fb105f4b32c9ad
wangchen <wangc1@email.arizona.edu>
2020-04-13 05:30:48 -0700

    updata something

--- setup/code/p3_feature.rb
+++ setup/code/p3_feature.rb
@@ -1,59 +1,63 @@
 type_Int = 10
 type_Float = 9.0
+type_arr = ["one","two","three","four","five","six"]
 type_Str = "It's #{type_Float} am."
-puts "#{type_Str}? No, it's #{type_Int} pm."
-puts "Is it 5 + 3 < 7? #{5 + 3 < 7}"
-title_arr =  "Here are some elements will add in array: "
-array_1 = "Dog "
-array_2 = "Cat "
-array_3 = "Eagle "
-array_4 = "Tiger "
-array_5 = "Koala "
-array_6 = "Panda "
-stuff_arr = [array_1,array_2,array_3,array_4,array_5,array_6]
+puts "Here are some data type:\n#{type_Str}? No, it's #{type_Int} pm. (Integer, Float and String)"
+puts "Is it 5 + 3 < 7? #{5 + 3 < 7} (Boolean)"
+puts "#{type_arr} (Array)"
 
 
-puts "A small-ish program."
+def error_type 
+    puts "\n\n\nNow show the error type."
+    begin  
+        puts 'Before the raise. '  
+        raise 'An error has occured.'  
+        puts 'After the raise.'
+    rescue  RuntimeError => e
+        puts 'It is a runtime error: raise \'An error has occured.\' '  
+    end 
 
-print "Now Type a Number (Integer): "
-get_int = gets.chomp.to_i
-puts "You input #{get_int} is a integer type.\n"
+    puts "It is a compile time type error: type_comp = \"*\" + 10.0\n\n "
+  end  
+error_type
 
-print "Type a Number (Float): "
-get_flo = gets.chomp.to_f
-puts "You input #{get_flo} is a float type.\n"
 
-print "Type a Sentence (String): "
-get_str = gets.chomp
-puts "You input #{get_str} is a string type.\n"
+def small_ish
+    title_arr =  "Here are some elements will add in array: "
+    array_1 = "Dog "
+    array_2 = "Cat "
+    array_3 = "Eagle "
+    array_4 = "Tiger "
+    array_5 = "Koala "
+    array_6 = "Panda "
+    stuff_arr = [array_1,array_2,array_3,array_4,array_5,array_6]
+    puts "A small-ish program."
 
-print "Answer the question: 5 + 3 is less than 7? (yes/no)? "
-get_str1 = gets.chomp
-puts "Your answer is #{get_str1 == "yes"}. This question perform a Boolean type\n"
+    print "Now Type a Number (Integer): "
+    get_int = gets.chomp.to_i
+    puts "You input #{get_int} is a integer type.\n"
 
-puts title_arr + array_1 + array_2 + array_3 + array_4 + array_5 + array_6
-print "But does not have enough elements need one more animal: "
-array_7 = gets.chomp
-puts "Adding: #{array_7}"
-stuff_arr.push(array_7)
-puts "Here we go: #{stuff_arr}"
+    print "Type a Number (Float): "
+    get_flo = gets.chomp.to_f
+    puts "You input #{get_flo} is a float type.\n"
 
-puts "Now show the error type."
-begin
-    type_strmore = '*' * 10
-    puts type_strmore
-    type_strmore = '10'.freeze
-    type_strmore << '56789'
-    puts type_strmore
-    
-rescue RuntimeError => e
-    print_exception(e, true)
-rescue => e
-    print_exception(e, false)
+    print "Type a Sentence (String): "
+    get_str = gets.chomp
+    puts "You input #{get_str} is a string type.\n"
+
+    print "Answer the question: 5 + 3 is less than 7? (yes/no)? "
+    get_str1 = gets.chomp
+    puts "Your answer is #{get_str1 == "yes"}. This question perform a Boolean type\n"
+
+    puts title_arr + array_1 + array_2 + array_3 + array_4 + array_5 + array_6
+    print "But does not have enough elements need one more animal: "
+    array_7 = gets.chomp
+    puts "Adding: #{array_7}"
+    stuff_arr.push(array_7)
+    puts "Here we go: #{stuff_arr}"
 end
 
+small_ish
 
-type_comp = "*" + 10.0
-puts type_comp
 
 
