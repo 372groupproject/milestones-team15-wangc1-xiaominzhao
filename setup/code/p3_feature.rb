0501d584c3a7aa2285a0b0a2e77c10fd90ab5e5b
wangchen <wangc1@email.arizona.edu>
2020-04-12 21:51:12 -0700

    Update p3_feature.rb

--- setup/code/p3_feature.rb
+++ setup/code/p3_feature.rb
@@ -3,8 +3,18 @@ type_Float = 9.0
 type_Str = "It's #{type_Float} am."
 puts "#{type_Str}? No, it's #{type_Int} pm."
 puts "Is it 5 + 3 < 7? #{5 + 3 < 7}"
+title_arr =  "Here are some elements will add in array: "
+array_1 = "Dog "
+array_2 = "Cat "
+array_3 = "Eagle "
+array_4 = "Tiger "
+array_5 = "Koala "
+array_6 = "Panda "
+stuff_arr = [array_1,array_2,array_3,array_4,array_5,array_6]
+
 
 puts "A small-ish program."
+
 print "Now Type a Number (Integer): "
 get_int = gets.chomp.to_i
 puts "You input #{get_int} is a integer type.\n"
@@ -21,6 +31,12 @@ print "Answer the question: 5 + 3 is less than 7? (yes/no)? "
 get_str1 = gets.chomp
 puts "Your answer is #{get_str1 == "yes"}. This question perform a Boolean type\n"
 
+puts title_arr + array_1 + array_2 + array_3 + array_4 + array_5 + array_6
+print "But does not have enough elements need one more animal: "
+array_7 = gets.chomp
+puts "Adding: #{array_7}"
+stuff_arr.push(array_7)
+puts "Here we go: #{stuff_arr}"
 
 puts "Now show the error type."
 begin
